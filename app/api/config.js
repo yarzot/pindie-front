514cd467bf5661576d129ab2f3406ec3859ab0b1
yarzot <yarozot71@gmail.com>
2024-05-24 22:09:09 +0300

    now

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL = 'https://shishkina.nomoredomainswork.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
