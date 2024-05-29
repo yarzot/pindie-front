fce9a46dfd0a56d35ec2f5ae95ec76d83edfd548
yarzot <yarozot71@gmail.com>
2024-05-29 18:16:53 +0300

    s

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://shishkina.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://ultra.nomorepartiesco.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
